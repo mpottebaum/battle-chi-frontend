393a08be6875af2f55f0336d5c58660a1c355237
mpottebaum <mpottebaum@gmail.com>
2020-05-15 14:29:50 -0500

    built GameInfo component

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class GameInfo extends React.Component {
-    render() {
-        return <div>
+    translateTurnStage = () => {
+        switch(this.props.game.turn_stage) {
+            case 0:
+                return 'Place Militias'
+            case 1:
+                return 'Attack'
+            case 2:
+                return 'Fortify'
+        }
+    }
 
+    findPlayer = () => {
+        return this.props.game.players.find(player => {
+            return player.order_num === this.props.game.order_num
+        })
+    }
+    
+    render() {
+        const player = this.findPlayer()
+        return <div>
+            <h3>Turn</h3>
+            <p>Number: {this.props.game.turn_num}</p>
+            <p>Player: {player.name}</p>
+            <p>Stage: {this.translateTurnStage()}</p>
         </div>
     }
 }
 
-export default GameInfo
+const mapStateToProps = state => {
+    return {
+        game: state.game
+    }
+}
+
+export default connect(mapStateToProps)(GameInfo)
