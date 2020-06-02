193a0ed1e9d7cb024416979c3930130f8f748847
mpottebaum <mpottebaum@gmail.com>
2020-06-02 10:38:19 -0500

    updated constants/index.js to recognize NODE_ENV

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,38 @@
-const API_ROOT = 'http://localhost:3000'
-export const API_WS_ROOT = 'ws://localhost:3000/cable'
+// const prod = {
+//   url: {
+//    API_URL: ‘https://myapp.herokuapp.com',
+//    API_URL_USERS: ‘https://myapp.herokuapp.com/users'}
+//  };
+//  const dev = {
+//   url: {
+//    API_URL: ‘http://localhost:3000'
+//   }
+//  };
+//  export const config = process.env.NODE_ENV === ‘development’ ? dev : prod;
+
+
+const prod = {
+  url: {
+    API_ROOT: 'https://whispering-crag-66572.herokuapp.com',
+    API_WS_ROOT: 'ws://whispering-crag-66572.herokuapp.com/'
+  }
+}
+
+const dev = {
+  url: {
+    API_ROOT: 'http://localhost:3000',
+    API_WS_ROOT: 'ws://localhost:3000/cable'
+  }
+}
+
+
+
+const API_ROOT = process.env.NODE_ENV === 'development' ? dev.url.API_ROOT : prod.url.API_ROOT;
+
+export const API_WS_ROOT = process.env.NODE_ENV === 'development' ? dev.url.API_WS_ROOT : prod.url.API_WS_ROOT;
+
+
+
 export const HEADERS = {
   'Content-Type': 'application/json',
   'Accept': 'application/json'
@@ -8,3 +41,5 @@ export const HEADERS = {
 export const gamesUrl = `${API_ROOT}/games`
 export const playersUrl = `${API_ROOT}/players`
 export const neighborhoodsUrl = `${API_ROOT}/neighborhoods`
+
+
